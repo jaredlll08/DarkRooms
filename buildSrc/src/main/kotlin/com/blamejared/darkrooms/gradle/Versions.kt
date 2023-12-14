6b2a1aad5b856f4eafb29d050a9d7666ff46a184
Jared <jaredlll08@gmail.com>
2023-12-13 17:10:09 -0700

    port to 1.20.2

--- buildSrc/src/main/kotlin/com/blamejared/darkrooms/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/darkrooms/gradle/Versions.kt
@@ -1,10 +1,10 @@
 package com.blamejared.darkrooms.gradle
 
 object Versions {
-    const val MOD = "2.0"
+    const val MOD = "3.0"
     const val JAVA = "17"
-    const val MINECRAFT = "1.20.1"
-    const val FORGE = "47.0.4"
-    const val FABRIC_LOADER = "0.14.21"
-    const val FABRIC = "0.83.1+1.20.1"
+    const val MINECRAFT = "1.20.2"
+    const val FORGE = "48.1.0"
+    const val FABRIC_LOADER = "0.15.1"
+    const val FABRIC = "0.91.2+1.20.2"
 }
